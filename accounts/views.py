57c493e2363572718fb48f4ea22a7ea5f3c7b44c
Kieu Thanh Nam <nam.uetn@gmail.com>
2023-10-24 10:06:40 +0700

    listing and detail restaurant, related category and fooditems

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,8 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.exceptions import PermissionDenied
 from django.db import transaction
 from django.utils.http import urlsafe_base64_decode
-from django.shortcuts import render, redirect\
+from django.shortcuts import render, redirect
+from django.template.defaultfilters import slugify
 
 from vendor.forms import VendorForm
 
@@ -67,7 +68,7 @@ def register_vendor(request):
     if request.user.is_authenticated:
         messages.warning(request, 'You are already logged in!')
 
-        return redirect('dashboard')
+        return redirect('myAccount')
     elif request.method == 'POST':
         form = UserForm(request.POST)
         v_form = VendorForm(request.POST, request.FILES)
@@ -85,6 +86,8 @@ def register_vendor(request):
 
             vendor = v_form.save(commit=False)
             vendor.user = user
+            vendor_name = v_form.cleaned_data['vendor_name']
+            vendor.vendor_slug = slugify(vendor_name) + '-' + str(user.id)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
